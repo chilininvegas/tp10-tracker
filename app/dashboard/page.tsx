04971695bb8d3d849171260ccc16f76c000f0e95
Chi Lin <chilin89117@gmail.com>
2025-11-30 22:45:35 -0800

    end of section 3 video 10

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+const DashboardPage = () => {
+  return (
+    <Link href='/dashboard/transactions/new'>New Transaction</Link>
+  )
+}
+
+export default DashboardPage
---
+++ components/UserDropDown.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import {useRouter} from 'next/navigation'
+import {UserButton} from '@clerk/nextjs'
+import { ChartColumnBig } from 'lucide-react'
+
+const UserDropDown = () => {
+  const router = useRouter()
+
+  return (
+    <UserButton showName={true} appearance={{
+      elements: {
+        userButtonOuterIdentifier: {
+          color: 'white'
+        }
+      }
+    }}
+    >
+      <UserButton.MenuItems>
+        <UserButton.Action
+          label='Dashboard'
+          labelIcon={<ChartColumnBig size={16} />}
+          onClick={() => router.push('/dashboard')}
+        />
+      </UserButton.MenuItems>
+    </UserButton>
+  )
+}
+
+export default UserDropDown
